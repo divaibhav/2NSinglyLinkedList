114afbae8a0f85352833bb0bc3973070bcb05171
Vaibhav Diwan <34906918+divaibhav@users.noreply.github.com>
2021-02-04 09:34:58 +0000

    declared removeFirst and removeLast method
    to delete node from linked list

--- src/myinterface/SinglyLinkedListADT.java
+++ src/myinterface/SinglyLinkedListADT.java
@@ -1,9 +1,14 @@
 package myinterface;
 
 public interface SinglyLinkedListADT {
-    void addFirst(int element);
-    boolean isEmpty();
-    int size();
-    void addLast(int element);
-    void traverse();
+    void addFirst(int element);//O(1)
+    boolean isEmpty();//O(1)
+    int size();//O(1)
+    void addLast(int element);//O(1)
+    void traverse();//O(N)
+    //deleteFromBeginning
+    //deleteFromHead
+    int removeFirst();//O(1)
+    //deleteFromEnd
+    int removeLast(); // O(N)
 }
